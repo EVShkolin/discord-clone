50f29c4bec97ec9ec1e38e4713ede1c94932aa16
Evgeniy <->
2026-05-08 01:00:53 +0300

    Remove context load in test

--- backend/src/test/java/ru/itis/backend/BackendApplicationTests.java
+++ backend/src/test/java/ru/itis/backend/BackendApplicationTests.java
@@ -5,12 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+//@SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-
-	}
+//	@Test
+//	void contextLoads() {
+//
+//	}
 
 }
